b99f89358d640c4fafcf186963be3d5ff1049d96
Rolf van de Krol <info@rolfvandekrol.nl>
2014-07-31 22:18:10 +0200

    install generator add route mount

--- lib/generators/page_tree/install_generator.rb
+++ lib/generators/page_tree/install_generator.rb
@@ -8,6 +8,15 @@ module PageTree
       def run_migrations
         run 'bundle exec rake db:migrate'
       end
+
+      def add_routes
+        insert_into_file File.join('config', 'routes.rb'), :after => "Rails.application.routes.draw do\n" do
+          %Q{
+  mount PageTree::Engine, :at => '/'
+
+}
+        end
+      end
     end
   end
 end
