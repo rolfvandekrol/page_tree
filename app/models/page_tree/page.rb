80cc8f1f3b69226b4b2fa226fca09c400c9bcc0d
Rolf van de Krol <info@rolfvandekrol.nl>
2014-07-31 22:15:20 +0200

    Add slug to page

--- app/models/page_tree/page.rb
+++ app/models/page_tree/page.rb
@@ -1,5 +1,9 @@
 module PageTree
   class Page < ActiveRecord::Base
+    extend FriendlyId
+
     acts_as_nested_set dependent: :destroy
+
+    friendly_id :title, use: [:slugged, :scoped], scope: :parent_id
   end
 end
--- lib/page_tree.rb
+++ lib/page_tree.rb
@@ -1,4 +1,5 @@
 require 'awesome_nested_set'
+require 'friendly_id'
 
 require "page_tree/engine"
 
