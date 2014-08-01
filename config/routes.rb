a70bcefb79e286d03edffaacba2e99c13bbb1c95
Rolf van de Krol <info@rolfvandekrol.nl>
2014-08-01 20:57:46 +0200

    Add page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 PageTree::Engine.routes.draw do
   root to: 'pages#show'
+  get :edit, to: 'pages#edit'
+  patch :update, to: 'pages#update'
+  put :update, to: 'pages#update'
+  delete :destroy, to: 'pages#destroy'
+  get :new, to: 'pages#new'
+  post :create, to: 'pages#create'
 end
