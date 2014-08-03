a8b4b243460772ddf0ed2493cb8e200ef70a3e2d
Rolf van de Krol <info@rolfvandekrol.nl>
2014-08-03 12:59:24 +0200

    Provides methods on controller

--- app/controllers/page_tree/pages_controller.rb
+++ app/controllers/page_tree/pages_controller.rb
@@ -6,5 +6,20 @@ module PageTree
 
     def show
     end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def create
+    end
+
+    def new
+    end
   end
 end
