bd4365532036c27fc246da5d3938bf86f7c6c826
Rolf van de Krol <info@rolfvandekrol.nl>
2014-08-03 13:32:02 +0200

    Finish new and edit form

--- app/controllers/page_tree/pages_controller.rb
+++ app/controllers/page_tree/pages_controller.rb
@@ -11,15 +11,42 @@ module PageTree
     end
 
     def update
+      respond_to do |format|
+        if @page.update_attributes(page_params)
+          format.html { redirect_to @page, notice: 'Page was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @page.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     def destroy
     end
 
     def create
+      @new_page = @page.children.new(page_params)
+      
+      respond_to do |format|
+        if @page.save
+          format.html { redirect_to @new_page, notice: 'Job was successfully created.' }
+          format.json { render json: [@page],
+                               status: :created, 
+                               location: [@page] }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @job.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     def new
+      @new_page = @page.children.build
+    end
+
+    def page_params
+      params.require(:page).permit(:title, :body)
     end
   end
 end
