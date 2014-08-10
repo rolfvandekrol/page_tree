d450ecb968bfde911759e1ec6299c01289d0280c
Rolf van de Krol <info@rolfvandekrol.nl>
2014-08-10 13:25:22 +0200

    Move all migrations together into one and remove body field

--- db/migrate/20140731145303_add_slug_to_page_tree_pages.rb
+++
@@ -1,9 +0,0 @@
-class AddSlugToPageTreePages < ActiveRecord::Migration
-  def change
-    change_table :page_tree_pages do |t|
-      t.string :slug
-
-      t.index [:parent_id, :slug], unique: true
-    end
-  end
-end
--- db/migrate/20140731145303_create_page_tree_pages.rb
+++ db/migrate/20140731145303_create_page_tree_pages.rb
@@ -2,9 +2,17 @@ class CreatePageTreePages < ActiveRecord::Migration
   def change
     create_table :page_tree_pages do |t|
       t.string :title
-      t.text :body
+
+      t.integer :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.integer :depth
+
+      t.string :slug
 
       t.timestamps
+
+      t.index [:parent_id, :slug], unique: true
     end
   end
 end
