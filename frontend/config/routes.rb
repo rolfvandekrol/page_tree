53eb39e0561752be0e130ea9614d7d170518521c
Rolf van de Krol <info@rolfvandekrol.nl>
2014-08-17 11:49:32 +0200

    Nicer syntax for scoping to page_tree/frontend module for routes

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -1,16 +1,14 @@
 PageTree::Frontend::Engine.routes.draw do
-  scope(module: 'page_tree') do
-    scope(module: 'frontend') do
-      get '/', to: 'pages#show'
-      get :edit, to: 'pages#edit'
-      get :new, to: 'pages#new'
-      
-      patch '/', to: 'pages#update'
-      put '/', to: 'pages#update'
-      delete '/', to: 'pages#destroy'
-      post '/', to: 'pages#create'
-      post :move_up, to: 'pages#move_up'
-      post :move_down, to: 'pages#move_down'
-    end
+  scope(module: 'page_tree/frontend') do
+    get '/', to: 'pages#show'
+    get :edit, to: 'pages#edit'
+    get :new, to: 'pages#new'
+    
+    patch '/', to: 'pages#update'
+    put '/', to: 'pages#update'
+    delete '/', to: 'pages#destroy'
+    post '/', to: 'pages#create'
+    post :move_up, to: 'pages#move_up'
+    post :move_down, to: 'pages#move_down'
   end
 end
